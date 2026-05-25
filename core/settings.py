e9369c09eb1514b784b0d5534557cbad839d6d6c
Ngokeng-Rayan <rayanngokeng6@gmail.com>
2026-05-25 16:07:16 +0100

    fix(db): utilisation de sqlite pour les tests et la ci, postgresql pour la production

--- core/settings.py
+++ core/settings.py
@@ -73,16 +73,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'cdiu8226_todoappbd',
-        'USER': 'cdiu8226_todoapp',
-        'PASSWORD': 'R@y@n2005',
-        'HOST': 'localhost',
-        'PORT': '5432',
+import os
+import sys
+
+if 'test' in sys.argv or os.environ.get('GITHUB_ACTIONS') == 'true':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'cdiu8226_todoappbd',
+            'USER': 'cdiu8226_todoapp',
+            'PASSWORD': 'R@y@n2005',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
+    }
 
 
 # Password validation
