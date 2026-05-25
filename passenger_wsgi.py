2385f3f40406245df57c7355a4eaefea7591813f
Ngokeng-Rayan <rayanngokeng6@gmail.com>
2026-05-25 16:31:35 +0100

    chore: test de diagnostic pour passenger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,12 +1,3 @@
-import os
-import sys
-
-# Ajouter le répertoire de base au chemin Python
-sys.path.insert(0, os.path.dirname(__file__))
-
-# Définir le module de configuration Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
-# Importer l'application WSGI de Django
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+def application(environ, start_response):
+    start_response('200 OK', [('Content-type', 'text/plain')])
+    return [b"Hello World from Passenger! Si tu vois ca, le serveur web marche !"]
